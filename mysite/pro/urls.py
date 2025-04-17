606ee4e5da1eb95f5872e3aa1bd1dbbb5b6272e7
rohiniramesh28 <169143654+rohiniramesh28@users.noreply.github.com>
2025-04-17 11:12:49 +0530

    Final push attempt

--- mysite/pro/urls.py
+++ mysite/pro/urls.py
@@ -1,6 +1,5 @@
-
 from django.urls import path
-
+from . import views  
 urlpatterns = [
-    path('loginpage/', views.loginpage,name='loginpage'),
+    path('loginpage/', views.loginpage, name='loginpage'),
 ]
